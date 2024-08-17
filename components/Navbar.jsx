928a4e3850fac9e38f946c667f5de646cf720c70
Sam Strike <sam.strike1@gmail.com>
2024-08-17 20:55:35 +0100

    Added name to navbar, changed experience section on portfolio and added tech section.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,8 +8,6 @@ import { LuFileBarChart } from "react-icons/lu";
 import { auth, signOut } from '@/auth';
 
 
-
-
 const navLinks = [
     {
         id: 1,
@@ -44,15 +42,16 @@ const Navbar = async () => {
     return (
         <div className='z-50'>
             <Container>
-                <div className='w-full flex flex-row my-5 items-center justify-between px-5'><h1 className={`text-5xl font-extrabold bg-gradient-to-r transition-all ${session?.user.role === 'admin' ? 'from-green-800 to-green-100' : 'from-blue-800 to-blue-100'}  text-transparent bg-clip-text text-left`}>Sam Strike</h1> {session?.user.role === 'admin' ? <ul className='flex flex-row gap-5'><Link href={'/admin/create-post'}>Create Post</Link> <Link href={'/admin'}>All Posts</Link> <form action={async () => {
+                <div className='w-full max-w-[1000px] flex flex-row my-5 items-center justify-between px-5'><h1 className={`text-5xl font-extrabold bg-gradient-to-r transition-all ${session?.user.role === 'admin' ? 'from-green-800 to-green-100' : 'from-blue-800 to-blue-100'}  text-transparent bg-clip-text text-left`}></h1> {session?.user.role === 'admin' ? <ul className='flex flex-row gap-5'><Link href={'/admin/create-post'}>Create Post</Link> <Link href={'/admin'}>All Posts</Link> <form action={async () => {
                     'use server'
                     await signOut({ redirectTo: "/" })
                 }}>
                     <button type='submit'>Logout</button>
                 </form> </ul> : ''}</div>
-                <div className='bg-base-100 p-[4px] bg-gradient-to-r transition-all from-blue-800 to-blue-400 rounded-3xl fixed w-[90%] bottom-5 lg:top-[95px] lg:h-fit z-50'>
+                <div className='bg-base-100 p-[4px] bg-gradient-to-r transition-all from-blue-800 to-blue-400 rounded-3xl fixed max-w-[1000px] w-[90%] bottom-[20px] lg:top-[20px] lg:h-fit z-50'>
 
                     <ul className='flex flex-row justify-evenly bg-base-100 rounded-[calc(1.5rem-4px)] h-[80px]'>
+                        <h4 className='h-full flex flex-row items-center font-extrabold text-3xl text-primary justify-start'>{"Sam Strike"}</h4>
                         {navLinks.map(item => (
                             <li key={item.id} className='cursor-pointer flex flex-row gap-2 items-center font-bold text-md sm:text-xl md:text-2xl hover:text-primary transition-colors'>
                                 <Link href={item.link} className='flex flex-row items-center gap-3'>
---
+++ components/PortfolioSectionTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PortfolioSectionTitle = ({ text }) => {
+    return (
+        <h2 className='text-center text-primary font-extrabold text-5xl mb-10 underline'>{text}</h2>
+    )
+}
+
+export default PortfolioSectionTitle
