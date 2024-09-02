6b4347e161adebce910672cc11ff4b60bd6790fd
Sam Strike <sam.strike1@gmail.com>
2024-09-02 11:05:08 +0100

    Created Footer component.

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from './Container'
+
+const Footer = () => {
+    return (
+        <footer className='mb-36 lg:mt-36 '>
+            <Container>
+                <span className='bg-primary text-white py-2 px-1 rounded-md'><a href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/" target="_blank" rel="noopener noreferrer">SVG Background</a></span>
+            </Container>
+
+        </footer>
+    )
+}
+
+export default Footer
