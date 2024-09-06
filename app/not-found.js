f42c4ee148fda58295398f172037fd755c683c49
Sam Strike <sam.strike1@gmail.com>
2024-09-06 14:44:35 +0100

    Changed text

--- app/not-found.js
+++ app/not-found.js
@@ -5,7 +5,7 @@ const page = () => {
     return (
         <div className='mb-36 lg:mt-36 h-[100vh]'>
             <Container>
-                <h2 className="font-bold text-2xl mb-10">Unfortunately the page you are looking for doesn't exist</h2>
+                <h2 className="font-bold text-2xl mb-10">{"Unfortunately the page you are looking for doesn't exist"}</h2>
                 <Link className="text-primary text-2xl" href={'/'}>Portfolio</Link>
             </Container>
 
