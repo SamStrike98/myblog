75c1950efd72332f0d71ae04bfee73469ce76b2d
Sam Strike <sam.strike1@gmail.com>
2024-09-06 14:39:56 +0100

    created 404 page.

---
+++ app/not-found.js
@@ -0,0 +1,17 @@
+import Container from "@/components/Container"
+import Link from "next/link"
+
+const page = () => {
+    return (
+        <div className='mb-36 lg:mt-36 h-[100vh]'>
+            <Container>
+                <h2 className="font-bold text-2xl mb-10">Unfortunately the page you are looking for doesn't exist</h2>
+                <Link className="text-primary text-2xl" href={'/'}>Portfolio</Link>
+            </Container>
+
+
+        </div>
+    )
+}
+
+export default page
