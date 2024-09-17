cfc01e884f7ea0a95522f9f84fb7975fbeb6510c
Sam Strike <sam.strike1@gmail.com>
2024-09-17 15:15:27 +0100

    Download CV button.

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ import Contact from '@/components/portfolio/Contact';
 
 const page = () => {
   return (
-    <div className='mb-36 lg:mt-36 '>
+    <div className='mb-36 lg:mt-36'>
 
       <Container>
 
@@ -26,6 +26,7 @@ const page = () => {
 
       </Container>
 
+      <div data-tip="Download a copy" className='tooltip tooltip-left text-white hover:bg-primary transition-colors fixed bottom-5 right-5 text-3xl font-extrabold border-2 border-primary rounded-full p-3'><a href='/cv.docx' download>CV</a></div>
     </div>
   )
 }
